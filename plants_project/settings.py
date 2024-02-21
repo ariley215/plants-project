03b53596bb71dbbad438408f018e9e6ade6b4b94
Andrea Riley <andreariley@me.com>
2024-02-21 17:13:42 -0500

    token authentication added

--- plants_project/settings.py
+++ plants_project/settings.py
@@ -145,5 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication", #default
+        "rest_framework.authentication.BasicAuthentication", #default
+    ],
 }
