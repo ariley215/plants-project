f24174d643ca6f537c21d93e3dc82e296504e8aa
Andrea Riley <andreariley@me.com>
2024-02-20 19:40:14 -0500

    permissions and postgres are added

--- plants/views.py
+++ plants/views.py
@@ -1,12 +1,15 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Plant
 from .seralizer import PlantSerializer
+from .permissions import IsOwnerOrReadOnly 
 
 class PlantListView(ListCreateAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class PlantDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
